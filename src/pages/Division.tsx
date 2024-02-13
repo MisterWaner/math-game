27899cac34e7ae5dca3295aba2b01c74fa30264d
Erwan Soulas <erwan.soulas@gmail.com>
2024-02-13 16:09:17 +0100

    change background and ended first changings of UI

--- src/pages/Division.tsx
+++ src/pages/Division.tsx
@@ -1,10 +1,20 @@
+import QuizScore from "../components/QuizScore/QuizScore";
 import Quizz from "../components/Quizz/Quizz";
+import Score from "../components/Score/Score";
 
 export default function Division() {
     return (
         <>
-            <h3>Division</h3>
-            <Quizz />
+            <main>
+                <div className="pageContainer">
+                    <div className="pageTitle">
+                        <h2>Division</h2>
+                    </div>
+                    <Quizz />
+                    <QuizScore />
+                </div>
+                <Score />
+            </main>
         </>
     );
 }
--- src/pages/Multiplication.tsx
+++ src/pages/Multiplication.tsx
@@ -1,10 +1,20 @@
+import QuizScore from "../components/QuizScore/QuizScore";
 import Quizz from "../components/Quizz/Quizz";
+import Score from "../components/Score/Score";
 
 export default function Multiplication() {
     return (
         <>
-            <h3>Multiplication</h3>
-            <Quizz />
+            <main>
+                <div className="pageContainer">
+                    <div className="pageTitle">
+                        <h2>Multiplication</h2>
+                    </div>
+                    <Quizz />
+                    <QuizScore />
+                </div>
+                <Score />
+            </main>
         </>
     );
 }
